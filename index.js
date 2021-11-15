f314cff08626e98b294c6a4a27d361d48e0d8187
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-11-16 00:39:57 +0600

    okk

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wquq5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4yggv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
